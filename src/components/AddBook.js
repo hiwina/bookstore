fd9f643dc12d0ecc07378fd28ab70a112250144f
hiwina <hiwina@gmail.com>
2023-06-23 16:27:44 +0300

    connect redux with react

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuid } from 'uuid';
+import { addBook } from '../redux/books/booksSlice';
 
-const AddBook = () => (
-  <div id="form-container">
-    <h2>Add new book</h2>
-    <form>
-      <input type="text" placeholder="Book title..." />
-      <input type="text" placeholder="Book Author" />
-      <button type="submit">Add Book </button>
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const addNewBook = (e) => {
+    e.preventDefault();
+    if (title !== '' && author !== '') {
+      dispatch(addBook({ book: { id: uuid(), title, author } }));
+      setTitle('');
+      setAuthor('');
+    }
+  };
+  return (
+    <>
+      <h2>Add new book</h2>
+      <form onSubmit={addNewBook}>
+        <input type="text" placeholder="Book title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" placeholder="Book Author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <button type="submit">Add </button>
+      </form>
+    </>
+  );
+};
 
 export default AddBook;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,28 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  bookItems: [
+    {
+      id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
-const bookSlice = createSlice({
+const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook: (state, action) => {
-      const { title, author } = action.payload;
-      const newBook = {
-        id: state.books.length + 1,
-        title,
-        author,
-      };
-      state.books.push(newBook);
+    addBook: (state, { payload }) => {
+      state.bookItems.push(payload.book);
     },
-    removeBook: (state, action) => {
-      const bookId = action.payload;
-      state.books = state.books.filter((book) => book.id !== bookId);
-    },
+    removeBook: (state, { payload }) => ({
+      bookItems: state.bookItems.filter((book) => book.id !== payload.id),
+    }),
   },
 });
 
-export const { addBook, removeBook } = bookSlice.actions;
-export default bookSlice.reducer;
+export const { addBook, removeBook } = booksSlice.actions;
+export default booksSlice.reducer;
